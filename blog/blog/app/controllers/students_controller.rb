4744892a0bcaab04e2e8f40e2b4dc6c2b6023ee5
kmcfly <kmcfly@126.com>
2017-02-14 18:20:03 +0800

    add video tag

--- blog/blog/app/controllers/students_controller.rb
+++ blog/blog/app/controllers/students_controller.rb
@@ -1,6 +1,8 @@
 class StudentsController < ApplicationController
 #  include ActionController::Live
   def evaluation
+    @micro_video_ids = Array.new(4)
+    @micro_video_ids = ["video_0","video_1","video_2","video_3"]
 =begin
     response.headers['Content-Type'] = 'text/event-stream'
     5.times {
