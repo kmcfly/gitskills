e37e85276d9bf803f7971d649cb9eb8865d145fd
kmcfly <kmcfly@126.com>
2017-02-14 15:34:19 +0800

    add video tag

--- blog/blog/app/controllers/students_controller.rb
+++ blog/blog/app/controllers/students_controller.rb
@@ -1,5 +1,15 @@
 class StudentsController < ApplicationController
+#  include ActionController::Live
   def evaluation
-
+=begin
+    response.headers['Content-Type'] = 'text/event-stream'
+    5.times {
+      response.stream.write "hello world\n"
+      sleep 1
+    }
+    ensure
+      response.stream.close
+=end
   end
+
 end
